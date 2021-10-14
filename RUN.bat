ac0ecc95c66a8babc195628904dbd6eec95a2804
Aaron Roethe <roethe.aaron@outlook.com>
2021-10-14 11:53:44 -0700

    update .bat location

--- RUN.bat
+++ RUN.bat
@@ -1,4 +1,5 @@
 set venv=emrr
+set projectname=call_campaign_emrr
 :: -------------------------------------------------------------------------
 :: Open anaconda & activate env
 :: -------------------------------------------------------------------------
@@ -11,6 +12,6 @@ cd %~dp0
 :: -------------------------------------------------------------------------
 :: Run script at this location
 :: -------------------------------------------------------------------------
-call %USERPROFILE%/Anaconda3/envs/%venv%/python.exe "%~dp0\main.py"
+call %USERPROFILE%/Anaconda3/envs/%venv%/python.exe "%~dp0\bin\%projectname%\main.py"
 PAUSE
 
