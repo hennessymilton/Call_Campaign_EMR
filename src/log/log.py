a639e925b69194e7420cb9b592f64021eb6da087
Aaron Roethe <roethe.aaron@outlook.com>
2021-12-30 10:32:29 -0700

    log module

---
+++ src/log/log.py
@@ -0,0 +1,15 @@
+import logging
+
+logging.basicConfig(filename='app.log',
+                    filemode='w',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
+                    level=logging.INFO,
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
+def df_len(df):
+    logging.info(f'len: {len(df)}')
+    # logging.info(df.dtypes)
+
+if __file__ == '__main__':
+    df = [1,2,3,4]
+    df_len(df)
