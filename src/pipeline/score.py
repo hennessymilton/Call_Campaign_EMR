6d50a96e91f900b5dd4c54528e6bb0c62f1e432d
Aaron Roethe <roethe.aaron@outlook.com>
2022-02-14 10:48:20 -0700

    rank & stack_inventory, complete remake

--- src/pipeline/score.py
+++ src/pipeline/score.py
@@ -1,67 +1,55 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 today = datetime.today()
+tomorrow = (today + timedelta(days = 1))
 
-### Calculate age from DaysSinceCreation & DaysSinceLC
-def age(df):
-    cols = ['InsertDate', 'Project Due Date', 'Last Call Date']
-    df[cols] = df[cols].apply(pd.to_datetime, errors='coerce')
+# def rank(df, grouping='PhoneNumber'):
+#     df.sort_values(['Skill', grouping,'meet_sla', 'has_call', 'togo_bin', 'age']
+#                ,ascending=[True, True, True, True, False, False], inplace=True)
+#     df['overall_rank'] = 1
+#     df['overall_rank'] = df.groupby(['Skill', grouping,])['overall_rank'].cumsum()
+#     f1 = df.overall_rank == 1
+#     df['parent'] = np.where(f1, 1, 0)
+#     df.sort_values(['Skill', 'parent','meet_sla', 'has_call', 'togo_bin', 'age']
+#                 ,ascending=[True, True, True, True, True, False, False], inplace=True)
+#     df.Score = 1
+#     df.Score = df.groupby(['Skill', 'parent'])['Score'].cumsum()
+#     df['Matchees'] = df.groupby([grouping])['OutreachID'].transform(lambda x : '|'.join(x)).apply(lambda x: x[:3000])
+#     return df
 
-    df['DaysSinceCreation'] = round((today - df['InsertDate'])/np.timedelta64(1,'D'))
-    df['DaysSinceLC'] = round((today - df['Last Call Date'])/np.timedelta64(1,'D'))
-
-    df['Last Call Date'] = df['Last Call Date'].dt.strftime('%Y-%m-%d')
-
-    filter1 = df['DaysSinceLC'] > df['DaysSinceCreation']
-    df['Age'] = np.where(filter1, df['DaysSinceCreation'], df['DaysSinceLC'])
+def rank(df, new_col, groups=list, rank_cols=dict):
+    sort_columns = groups + [*rank_cols.keys()]
+    ascending    = [True] * len(groups) + [*rank_cols.values()]
+    
+    df.sort_values(sort_columns,ascending=ascending, inplace=True)
+    df[new_col] = 1
+    df[new_col] = df.groupby(groups)[new_col].cumsum()
     return df
 
-### Ranking ###
-#   Medicare Risk   = 1
-#   Medicaid Risk   = 2
-#   RADV            = 3
-#   HEDIS           = 4
-#   Specialty       = 6
-#   ACA             = 17
+def stack_inventory(df, grouping):
+    rank_cols = {'pend':True,'meet_sla':True,'no_call':False, 'togo_bin':False, 'age':False}
 
-def cc_score_deduplicate(df):
-    audit_sort = {3:0, 2:1, 4:2, 6:3,  17:4, 1:5}
-    df['audit_sort'] = df['Audit Type'].map(audit_sort)
+    # group by phone number or msid & rank highest value org
+    df = rank(df,'overall_rank',['Skill', grouping], rank_cols)
 
-    df2 = df.groupby(['Phone Number']).agg({'bin':'mean', 'ToGo Charts':'sum', 'Age':'mean'}).rename(columns={'bin':'bin_agg', 'ToGo Charts':'togo_agg','Age':'age_avg'}).reset_index()
-    skilled = pd.merge(df,df2, on='Phone Number', how='left')
+    # top overall per group = parent
+    f1 = df.overall_rank == 1
+    df['parent'] = np.where(f1, 1, 0)
 
-    skilled['coef'] = skilled['bin_agg'] / skilled['togo_agg']
-    skilled['coef'] = skilled['coef'].fillna(max(skilled['coef']))
-    skilled['bin_coef'] = pd.qcut(skilled['coef'], 3, labels= range(1,4))
-    skilled['bin_coef'] = skilled['bin_coef'].astype(int)
-    df_rank = skilled.sort_values(by = ['Phone Number', 'audit_sort', 'bin']).reset_index(drop= True)
+    # re-rank parent orgs
+    df = rank(df,'Score' , ['Skill','parent'], rank_cols)
+    df.OutreachID = df.OutreachID.astype(str)
+    df['Matchees'] = df.groupby([grouping])['OutreachID'].transform(lambda x : '|'.join(x)).apply(lambda x: x[:3000])
+    return df
 
-    # Parent / Child
-    df_rank['Unique_Phone'] = 'Child'
-    df_unique = df_rank.drop_duplicates(['Phone Number']).reset_index(drop = True)
-    df_unique['Unique_Phone'] = 'Parent'
-    df_unique = df_unique.sort_values(by = ['audit_sort','bin_coef', 'age_avg'], ascending=[True, True, False]).reset_index(drop= True)
-
-    df_unique['rank'] = range(0, len(df_unique))
-
-    # Add Unique ORGs to Rank list 
-    df_full = df_unique.append(df_rank)
-    df_clean = df_full.drop_duplicates(['OutreachID']).reset_index(drop= True)
-
-    ### Piped ORGs attached to phone numbers
-    df_clean['OutreachID'] = df_clean['OutreachID'].astype(str)
-    df_clean['Matches'] = df_clean.groupby(['Phone Number'])['OutreachID'].transform(lambda x : '|'.join(x)).apply(lambda x: x[:3000])
-    return df_clean
-
-def pt_score(df):
-      df['Today\'s Targeted charts'] =df['Today\'s Targeted charts'].replace(0,1)
-      df = df.groupby(['Project Type']).agg({'Today\'s Targeted charts':'sum', 'QA Completed':'sum', 'SB.EMR Remote':'sum'})
-      df['coef'] = df['QA Completed'] / df['Today\'s Targeted charts'] * df['SB.EMR Remote']
-      df = df.sort_values(by= ['coef'], ascending=True).reset_index().round()#.astype(int)
-      df['rank'] = range(0, len(df))
-      df['bin'] = pd.qcut(df['rank'], 3,  labels= range(1,4)) #,labels=["good", "medium", "bad"])
-      df['bin'] = df['bin'].astype(int)
-      df = df[['Project Type', 'bin']]
-      return df
+# def pt_score(df):
+#       df['Today\'s Targeted charts'] =df['Today\'s Targeted charts'].replace(0,1)
+#       df = df.groupby(['Project Type']).agg({'Today\'s Targeted charts':'sum', 'QA Completed':'sum', 'SB.EMR Remote':'sum'})
+#       df['coef'] = df['QA Completed'] / df['Today\'s Targeted charts'] * df['SB.EMR Remote']
+#       df = df.sort_values(by= ['coef'], ascending=True).reset_index().round()#.astype(int)
+#       df['rank'] = range(len(df))
+#       df['bin'] = pd.qcut(df['rank'], 3,  labels= range(1, 4))
+#       df['bin'] = df['bin'].astype(int)
+#       df = df[['Project Type', 'bin']]
+#       return df
